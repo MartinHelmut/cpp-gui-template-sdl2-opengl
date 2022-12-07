d0af56229eca8f583c995cf56314076cab23a159
Martin Helmut Fieber <info@martin-fieber.se>
2022-12-07 10:05:58 +0100

    Fix building and running app on Windows

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -4,11 +4,13 @@ if (NOT CMAKE_BUILD_TYPE STREQUAL "Release")
     message(STATUS "Using clang-tidy")
     set(CMAKE_CXX_CLANG_TIDY ${CLANGTIDY})
   else ()
-    message(SEND_ERROR "clang-tidy requested but executable not found")
+    message(WARNING "clang-tidy requested but executable not found")
   endif ()
 
   # This will gradually increase memory usage of the program,
   # running on Apple M1, 13.0.
-  message(STATUS "Using address sanitizer")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -fsanitize=address -g")
+  if (NOT WIN32)
+    message(STATUS "Using address sanitizer")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -fsanitize=address -g")
+  endif ()
 endif ()
